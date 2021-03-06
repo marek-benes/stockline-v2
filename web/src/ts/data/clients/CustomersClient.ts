612bfcdc4820526a17814effc9c864d551fcb3d4
Martin Itterheim <itterheim.martin@gmail.com>
2021-03-06 14:44:42 +0100

    fix: customers client

--- web/src/ts/data/clients/CustomersClient.ts
+++ web/src/ts/data/clients/CustomersClient.ts
@@ -22,7 +22,7 @@ export class CustomersClient {
 
     public async getCustomers (options?: ICustomerRequest): Promise<ICustomer[]> {
         if (!this.cachedCustomers) {
-            this.cachedCustomers = new CachedData(async () => this.httpClient.get<IDataset<ICustomer[]>>(this.api.data + `/datasets/customers${this.createQueryString(options)}`, this.httpHeaders));
+            this.cachedCustomers = new CachedData(async () => this.httpClient.get<IDataset<ICustomer[]>>(this.api.data + `/datasets/customers${this.httpClient.createQueryString(options)}`, this.httpHeaders));
         }
         const customers = await this.cachedCustomers.getData();
         return customers.data;
@@ -31,25 +31,5 @@ export class CustomersClient {
     public async getCustomer (id: string): Promise<ICustomer> {
         const customers = await this.httpClient.get<IDataset<ICustomer[]>>(this.api.data + `/datasets/customers?id=${id}`, this.httpHeaders);
         return customers?.data[0];
-    }
-
-    private createQueryString (options?: object): string {
-        if (!options) { return ""; }
-
-        let result = "?";
-
-        result += Object.keys(options).map((key) => {
-            const value = options[key];
-
-            if (Array.isArray(value)) {
-                return `${key}=${value.join(",")}`;
-            } else if (typeof key === "object") {
-                return `${key}=${JSON.stringify(value)}`;
-            }
-
-            return `${key}=${value}`;
-        }).join("&");
-
-        return result;
     }
 }
